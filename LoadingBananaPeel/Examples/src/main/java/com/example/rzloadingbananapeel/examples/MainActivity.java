ee1ae78a469e1c68918a0bd6ef0c6ae90ef5c8a8
Stephen Barnes <sbarnes@fitbit.com>
2014-11-09 16:29:16 -0500

    Update for Android L
     - Move to build tools 15
     - Set the minimum SDK to 14
     - Move to using the android support card view
     - Use L's primary color to theme the banana peel
     - Remove the custom run on UI in exchange for post()

---
+++ LoadingBananaPeel/Examples/src/main/java/com/example/rzloadingbananapeel/examples/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.rzloadingbananapeel.examples;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.smb.loadingbananapeel.LoadingBananaPeelView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_about) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        private final static int INTERVAL = 5000; //  5000ms = 5 seconds
+
+        LoadingBananaPeelView mLoadingBananaPeelView;
+        Timer pseudoContentDelay = new Timer("PseudoContentDelay", true);
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            mLoadingBananaPeelView = (LoadingBananaPeelView)rootView.findViewById(R.id.fragment_content_loading_view);
+
+            mLoadingBananaPeelView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mLoadingBananaPeelView.isShowingContentView()) {
+                        mLoadingBananaPeelView.showBananaPeel();
+                    } else {
+                        mLoadingBananaPeelView.showContent();
+                    }
+                }
+            });
+
+            return rootView;
+        }
+
+        @Override
+        public void onViewCreated(View view, Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            mLoadingBananaPeelView.showLoading();
+
+            pseudoContentDelay.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    mLoadingBananaPeelView.showContent();
+                }
+            }, 5000);
+        }
+    }
+}
